9be07407b89c2d53a12c3e4d8174e62b30359e8b
Добрый <62813878+WinstonLog@users.noreply.github.com>
2026-04-12 23:39:23 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js - НЕ добавляйте в Git!
+const SUPABASE_CONFIG = {
+    URL: 'https://tixgzuqpdepyxgueriqw.supabase.co',
+    ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpeGd6dXFwZGVweXhndWVyaXF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NzAzMjIsImV4cCI6MjA5MTM0NjMyMn0.sskc8yr9bfHuCzAMVpupM7dJWXhfqg2GWg-1StQcQDM'
+};
+
+const GAME_CONFIG = {
+    PACK_PRICE: 70,
+    START_CRYSTALS: 40,
+    STORAGE_KEY: 'bloody_ssr_v18_promo',
+    DUPLICATE_REWARD_CRYSTALS: 7
+};
